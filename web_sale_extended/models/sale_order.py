3e4065d22b2924a0b2bf573fe93a0a51bc08caf6
Jhair Escobar <je@todoo.co>
2021-03-15 09:00:59 -0500

    [FIX] Actualizacion desde confacturación

--- web_sale_extended/models/sale_order.py
+++ web_sale_extended/models/sale_order.py
@@ -140,65 +140,6 @@ class SaleOrder(models.Model):
                         )
                         order.message_post(body=body_message, type="comment")
                         
-                        
-                        
-                        
-                    # EMAIL!!! (subir)
-                    #record.action_quatition_send()
-                    #template = request.env.ref('web_sale_extended_template_sale_update',
-                    #                       raise_if_not_found=False)
-                    #template_id = self.env['mail.template'].search([('tusdatos_confirmation_accept', '=', True)], limit=1)
-                    """
-                    template_id = self._find_mail_template(force_confirmation_template=True)
-                    context = dict(self.env.context)
-                    if template_id:
-                        sale_id.with_context(force_send=True).message_post_with_template(
-                            int(template_id), composition_mode='comment', email_layout_xmlid="mail.mail_notification_paynow")
-                    """    
-                        
-                        
-                        """
-                        _logger.error('***************************** TUS DATOS CONFIRMACION ++++++++++++++++++++++++++++++++++')
-                        _logger.error(template)
-                        
-                        
-                        #template_values = template.generate_email(sale_id.id, fields=None)
-                        template_values = {}
-                        _logger.error('***************************** 777777777777777777777 ++++++++++++++++++++++++++++++++++')
-                        _logger.error(template_values)
-                        ctx = {
-                            'default_model': 'sale.order',
-                            'default_res_id': sale_id.id,
-                            'default_use_template': bool(template.id),
-                            'default_template_id': template.id,
-                            'default_composition_mode': 'comment',
-                            'mark_so_as_sent': True,
-                            'custom_layout': "mail.mail_notification_paynow",
-                            'proforma': self.env.context.get('proforma', False),
-                            'force_email': True,
-                            'model_description': self.with_context(lang=self.env.user.lang).type_name,
-                        }
-                        body = template.with_context(ctx)._render_template(template.body_html, 'sale.order', sale_id.id)
-                        _logger.error(body)
-                        template_values.update({
-                            #'email_to': sale_id.tusdatos_email,
-                            #'email_to': sale_id.partner_id.email,
-                            'auto_delete': False,
-                            'partner_to': sale_id.partner_id.id,
-                            'scheduled_date': False,
-                            'body_html': body,
-                        })
-                        
-                        
-                        if "partner_ids" in template_values:
-                            template_values.pop('partner_ids')
-                        template.write(template_values)
-                        cleaned_ctx = dict(self.env.context)
-                        cleaned_ctx.pop('default_type', None)
-                        cleaned_ctx['custom_layout'] = 'mail.mail_notification_paynow'
-                        cleaned_ctx['mark_so_as_sent'] = True
-                        template.with_context(cleaned_ctx).send_mail(sale_id.id, force_send=True, raise_exception=True)
-                        """
                 else:
                     if approval[1] and 'estado' in approval[1]:
                         if approval[1]['estado'] in ('error, tarea no valida'):
@@ -212,24 +153,6 @@ class SaleOrder(models.Model):
                         en las lista Onu o OFAC"""
                         sale_id.write({'tusdatos_request_expired' : True,})
                         sale_id.message_post(body=message)
-                        """
-                        _logger.error('***************************** ENVIANDO CORREO DE RESPUESTA NEGATIVA  ++++++++++++++++++++++++++++++++++')
-                        template = self.env['mail.template'].search([('tusdatos_confirmation_reject', '=', True)], limit=1)
-                        context = dict(self.env.context)
-                        if template:
-                            template_values = template.generate_email(sale_id.id, fields=None)
-                            template_values.update({
-                                #'email_to': sale_id.tusdatos_email,
-                                'email_to': sale_id.partner_id.email,
-                                'auto_delete': False,
-                                #'partner_to': False,
-                                'scheduled_date': False,
-                            })
-                            template.write(template_values)
-                            cleaned_ctx = dict(self.env.context)
-                            cleaned_ctx.pop('default_type', None)
-                            template.with_context(lang=self.env.user.lang).send_mail(sale_id.id, force_send=True, raise_exception=True)
-                        """
             """Aseguramos que las transacciones ocurren cada 5 segundos"""
             time.sleep(6)
                     
