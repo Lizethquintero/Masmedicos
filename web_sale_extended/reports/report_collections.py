a51cc9d30c012779f3a80e244bd8dfb9ad9896be
FelipeNavasTodoo <ln@todoo.co>
2021-06-30 11:04:25 -0500

    [FIX] archivo de recaudos information

--- web_sale_extended/reports/report_collections.py
+++ web_sale_extended/reports/report_collections.py
@@ -71,9 +71,9 @@ class CollectionsReportLine(models.Model):
         sub.recurring_monthly as collected_value,        
         '1'::text as number_of_installments,        
         sorder.payment_method_type as payment_method,
-        subtmpl.recurring_rule_count / subtmpl.recurring_interval as number_of_plan_installments,
-        '4'::text as total_installments,
-        '1'::text as number_of_installments_arrears        
+        subtmpl.subscription_duration / subtmpl.recurring_interval as number_of_plan_installments,
+        ''::text as total_installments,
+        ''::text as number_of_installments_arrears        
         
         from sale_subscription sub
         left join res_partner p on p.subscription_id = sub.id
